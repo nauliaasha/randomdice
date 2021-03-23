e66a87ca2c8c90b6ab4ededd9cb5687f558163d4
Aulia Asha Normawati <68146729+nauliaasha@users.noreply.github.com>
2021-03-23 23:53:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ image1.setAttribute("src", randomDiceImage1);
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
 
-document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomDiceImage2);
 
 
 if (randomNumber1 > randomNumber2) {
